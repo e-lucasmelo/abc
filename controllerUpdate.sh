e2a75e95bc25e601a9c4424149ad9e5400ead998
e-lucas <e.lucasmelo@outlook.com>
2025-02-02 13:39:54 -0300

    novo arquivo

---
+++ controllerUpdate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Carregando variáveis de ambiente do OpenStack..."
+. admin-openrc
+
+echo "verificando os hosts compute..."
+sudo nova-manage cell_v2 discover_hosts --verbose
+
+echo "verificar a lista de serviços, catálogo e imagem"
+openstack compute service list
+openstack catalog list
+openstack image list
+sudo nova-status upgrade check
+
+echo "Verificando extensões de rede e agentes de rede do Neutron..."
+openstack extension list --network
+openstack network agent list
+
+echo "configuração concluída!"
