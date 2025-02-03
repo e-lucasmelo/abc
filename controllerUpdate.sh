60e04402bcbd793b416fb0fe03eb8b5761abdfc8
e-lucas <e.lucasmelo@outlook.com>
2025-02-03 09:13:36 -0300

    atualizacao

--- controllerUpdate.sh
+++ controllerUpdate.sh
@@ -16,4 +16,7 @@ echo "Verificando extensões de rede e agentes de rede do Neutron..."
 openstack extension list --network
 openstack network agent list
 
+echo "reiniciando todos os serviços...."
+sudo systemctl restart apache2.service glance-api.service neutron-dhcp-agent.service neutron-l3-agent.service neutron-metadata-agent.service neutron-openvswitch-agent.service neutron-server.service nova-api.service nova-compute.service nova-conductor.service nova-novncproxy.service nova-scheduler.service nova-api.service cinder-scheduler.service
+
 echo "configuração concluída!"
