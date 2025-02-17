6e4e514b2b21f72acf01a12e1521b4293208dd9e
e-lucas <e.lucasmelo@outlook.com>
2025-02-17 20:08:32 -0300

    atualizacao

--- controller_rings.sh
+++ controller_rings.sh
@@ -3,7 +3,7 @@
 source variaveis.sh
 
 # Criar o account ring
-sudo swift-ring-builder /etc/swift/account.builder create 10 3 1
+sudo swift-ring-builder /etc/swift/account.builder create 10 2 1
 sleep 2
 # ajustar o if para validar todos os discos corretamente
 if [ -z "disk_object1" ]; then
@@ -22,7 +22,7 @@ sudo swift-ring-builder /etc/swift/account.builder rebalance
 sleep 2
 #container
 
-sudo swift-ring-builder /etc/swift/container.builder create 10 3 1
+sudo swift-ring-builder /etc/swift/container.builder create 10 2 1
 sleep 2
 sudo swift-ring-builder /etc/swift/container.builder   add --region 1 --zone 1 --ip ${object1[1]} --port 6201   --device $disk_object1 --weight 100
 sudo swift-ring-builder /etc/swift/container.builder   add --region 1 --zone 1 --ip ${object1[1]} --port 6201   --device $disk_object2 --weight 100
@@ -31,7 +31,7 @@ sleep 2
 sudo swift-ring-builder /etc/swift/container.builder
 #object
 
-sudo swift-ring-builder /etc/swift/object.builder create 10 3 1
+sudo swift-ring-builder /etc/swift/object.builder create 10 2 1
 sleep 2
 sudo swift-ring-builder /etc/swift/object.builder   add --region 1 --zone 1 --ip ${object1[1]} --port 6200   --device $disk_object1 --weight 100
 sudo swift-ring-builder /etc/swift/object.builder   add --region 1 --zone 1 --ip ${object1[1]} --port 6200   --device $disk_object2 --weight 100
