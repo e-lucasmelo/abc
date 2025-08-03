4c1d57b085b62541b08944b2ff67eb877fc3dc9b
e-lucas <e.lucasmelo@outlook.com>
2025-08-03 18:14:09 -0300

    atualizacao parametros do cinder e download do rabbitmq

--- config_hosts_v8.sh
+++ config_hosts_v8.sh
@@ -797,6 +797,36 @@ echo "Reiniciando o serviço MariaDB..."
 sudo service mysql restart
 
 
+echo "download da chave assinada do rabbitmq..."
+## Team RabbitMQ's signing key
+curl -1sLf "https://keys.openpgp.org/vks/v1/by-fingerprint/0A9AF2115F4687BD29803A206B73A36E6026DFCA" | sudo gpg --dearmor | sudo tee /usr/share/keyrings/com.rabbitmq.team.gpg > /dev/null
+
+## Add apt repositories maintained by Team RabbitMQ
+sudo tee /etc/apt/sources.list.d/rabbitmq.list <<EOF
+## Modern Erlang/OTP releases
+##
+deb [arch=amd64 signed-by=/usr/share/keyrings/com.rabbitmq.team.gpg] https://deb1.rabbitmq.com/rabbitmq-erlang/ubuntu/noble noble main
+deb [arch=amd64 signed-by=/usr/share/keyrings/com.rabbitmq.team.gpg] https://deb2.rabbitmq.com/rabbitmq-erlang/ubuntu/noble noble main
+
+## Latest RabbitMQ releases
+##
+deb [arch=amd64 signed-by=/usr/share/keyrings/com.rabbitmq.team.gpg] https://deb1.rabbitmq.com/rabbitmq-server/ubuntu/noble noble main
+deb [arch=amd64 signed-by=/usr/share/keyrings/com.rabbitmq.team.gpg] https://deb2.rabbitmq.com/rabbitmq-server/ubuntu/noble noble main
+EOF
+
+## Update package indices
+sudo apt update -y
+
+## Install Erlang packages
+sudo apt install -y erlang-base \
+                        erlang-asn1 erlang-crypto erlang-eldap erlang-ftp erlang-inets \
+                        erlang-mnesia erlang-os-mon erlang-parsetools erlang-public-key \
+                        erlang-runtime-tools erlang-snmp erlang-ssl \
+                        erlang-syntax-tools erlang-tftp erlang-tools erlang-xmerl
+
+## Install rabbitmq-server and its dependencies
+sudo apt install rabbitmq-server -y --fix-missing
+
 # Instalar o RabbitMQ
 echo "Instalando o RabbitMQ..."
 sudo apt install rabbitmq-server -y &>/dev/null
